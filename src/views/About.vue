16871a72ef5705be1564fefa71c1c3dd924261ae
Maria Sorokina <113168296+bijou743@users.noreply.github.com>
2022-10-11 22:55:16 +0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="about">
+		<transition
+			appear
+			@before-enter="beforeEnter"
+			@enter="enter"
+			@afterEnter="afterEnter"
+		>
+			<h1>About</h1>
+		</transition>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+			officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+			quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+			assumenda nam!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+			officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+			quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+			assumenda nam!
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+			officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+			quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+			assumenda nam!
+		</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import gsap from 'gsap';
+
+export default {
+	setup() {
+		const beforeEnter = (el) => {
+			el.style.transform = 'translateY(-60px)';
+			el.style.opacity = 0;
+		};
+
+		const enter = (el, done) => {
+			// animation
+			gsap.to(el, {
+				duration: 1,
+				y: 0,
+				opacity: 1,
+				ease: 'bounce.out',
+				onComplete: done /* to start afterEnter after enter ends */,
+			});
+		};
+
+		const afterEnter = (el) => {};
+
+		return {
+			beforeEnter,
+			enter,
+			afterEnter,
+		};
+	},
+};
+</script>
+
+<style>
+.about {
+	max-width: 600px;
+	margin: 20px auto;
+}
+</style>
